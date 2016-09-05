af6fe72b8a5db178fbca619c4735edcd8bc31abc
ggyyeh <gy_eh@yahoo.com>
2016-09-05 10:53:56 +0300

    updated readme with images

--- kodiplugins/plugin.video.exodus/exodus.py
+++ kodiplugins/plugin.video.exodus/exodus.py
@@ -25,6 +25,7 @@ import xbmcaddon, xbmc
 __settings__ = xbmcaddon.Addon(id='plugin.video.exodus')
 __PLUGIN_PATH__ = __settings__.getAddonInfo('path')
 LIB_PATH = xbmc.translatePath( os.path.join( __PLUGIN_PATH__, 'resources', 'lib' ) )
+print LIB_PATH
 sys.path.append (LIB_PATH)
 
 params = dict(urlparse.parse_qsl(sys.argv[2].replace('?','')))
