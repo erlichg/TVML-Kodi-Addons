be9f55edb36b0110b2450fc635dcf3a2a99c3104
ggyyeh <gy_eh@yahoo.com>
2016-09-06 10:48:11 +0300

    mmain method

--- app.py
+++ app.py
@@ -253,10 +253,11 @@ def get_menu(bridge, plugin, url):
 def is_ascii(s):
 	return all(ord(c) < 128 for c in s)
 
-
-		
-if __name__ == '__main__':
+def mmain():
 	http_server = WSGIServer(('',5000), app)
 	#http_server.log = open('http.log', 'w')
 	http_server.serve_forever()
 	#app.run(debug=True, host='0.0.0.0')
+		
+if __name__ == '__main__':
+	mmain()
